b1042b4bc172f3c25f1eca044f4b5ffa4c079837
RtnMDRam <rtnmdramamoorthy@gmail.com>
2025-10-11 20:00:24 +0530

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,3 +3,10 @@ from .theme import apply_theme
 from .io_utils import read_bilingual, export_qc
 from .glossary import render_matches, sort_glossary
 from .qc_state import auto_guess_map, ensure_work, step_columns
+
+__all__ = [
+    "apply_theme",
+    "read_bilingual", "export_qc",
+    "render_matches", "sort_glossary",
+    "auto_guess_map", "ensure_work", "step_columns",
+]
